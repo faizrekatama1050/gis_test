4db8ab00f292afa5931a53cf48f1b6ff7b332dc4
faizrekatama <faizrekatama@gmail.com>
2018-03-29 10:03:02 +0700

    test

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <style>
+            #map {
+                width: 100%;
+                height: 500px;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="map"></div>
+        <script>
+            
+        </script>
+    </body>
+</html>
